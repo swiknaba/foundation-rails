f4d7d446371d6b8933134e0e98f6b2104f79a5cf
Lud <swiknaba@users.noreply.github.com>
2023-04-30 23:21:40 +0200

    no need for require foundation anymore in JS

--- spec/features/generator_spec.rb
+++ spec/features/generator_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 feature 'Foundation install succeeds' do
-  scenario 'stylesheets assets files are added' do
+  scenario 'stylesheets assets files are added', :aggregate_failures do
     application_css_file = IO.read("#{dummy_app_path}/app/assets/stylesheets/application.css")
 
     expect(File).to exist("#{dummy_app_path}/app/assets/stylesheets/_settings.scss")
@@ -9,14 +9,13 @@ feature 'Foundation install succeeds' do
     expect(application_css_file).to match(/require foundation_and_overrides/)
   end
 
-  scenario 'javascripts assets files are added' do
+  scenario 'javascripts assets files are added', :aggregate_failures do
     application_js_file = IO.read("#{dummy_app_path}/app/assets/javascripts/application.js")
 
-    expect(application_js_file).to match(/require foundation/)
     expect(application_js_file).to match(Regexp.new(Regexp.escape('$(function(){ $(document).foundation(); });')))
   end
 
-  scenario 'layout file loads assets' do
+  scenario 'layout file loads assets', :aggregate_failures do
     layout_file = IO.read("#{dummy_app_path}/app/views/layouts/application.html.erb")
 
     expect(layout_file).to match(/stylesheet_link_tag    "application"/)
